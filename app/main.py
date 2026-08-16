99368f8b232d99e66062f8e60696bfc9fd4087e2
Daniel Mamak <daniel.mamak@gmail.com>
2026-08-16 19:24:34 +0200

    Stwórz klase Person z metodą init

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 class Person:
-    # write your code here
-    pass
 
+    people = {}
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
