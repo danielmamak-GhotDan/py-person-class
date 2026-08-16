a51e78d50e44325f728f7d7efc810027cfa36ac9
Daniel Mamak <daniel.mamak@gmail.com>
2026-08-16 20:32:40 +0200

    Przypisz malzonkow

--- app/main.py
+++ app/main.py
@@ -13,3 +13,16 @@ def create_person_list(people: list) -> list:
     for person in people:
         new_person = Person(person["name"], person["age"])
         list_person.append(new_person)
+
+    for person in people:
+        new_obj = Person.people[person["name"]]
+
+        if "wife" in person and person["wife"] is not None:
+            new_wife = person["wife"]
+            new_obj.wife = Person.people[new_wife]
+
+        if "husband" in person and person["husband"] is not None:
+            new_husband = person["husband"]
+            new_obj.husband = Person.people[new_husband]
+
+    return list_person
