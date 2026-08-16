98e158f754853e94aafe2118efa5e5773d508e19
Daniel Mamak <daniel.mamak@gmail.com>
2026-08-16 19:40:03 +0200

    Dodaj funkcję create_person_list

--- app/main.py
+++ app/main.py
@@ -6,3 +6,10 @@ class Person:
         self.name = name
         self.age = age
         Person.people[self.name] = self
+
+
+def create_person_list(people: list) -> list:
+    list_person = []
+    for person in people:
+        new_person = Person(person["name"], person["age"])
+        list_person.append(new_person)
