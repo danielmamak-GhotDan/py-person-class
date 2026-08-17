8e9e6bc5050532849a7b7eb999af1088bf34fbed
Daniel Mamak <daniel.mamak@gmail.com>
2026-08-17 19:56:19 +0200

    Dodać docstring

--- app/main.py
+++ app/main.py
@@ -9,6 +9,10 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    """
+    Zamienia listę słowników z danymi osób na listę
+    instancji klasy Person.
+    """
     list_person = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
